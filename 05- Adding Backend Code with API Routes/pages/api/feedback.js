94c81c35824a4a3f364c405a440e869f1eb18122
hagleyson <hagleyson@gmail.com>
2022-03-15 16:42:20 -0300

    project time api routes

---
+++ 05- Adding Backend Code with API Routes/pages/api/feedback.js
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fs from "fs";
+import path from "path";
+
+export function buildFeedbackPath() {
+  return path.join(process.cwd(), "data", "feedback.json");
+}
+
+export async function extractFeedback(filePath) {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+}
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+    let newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedbackText,
+    };
+    // store that in a database or in a file
+    const filePath = buildFeedbackPath();
+    let data = await extractFeedback(filePath);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: "Success!", feedback: newFeedback });
+  } else {
+    const filePath = buildFeedbackPath();
+    const data = await extractFeedback(filePath);
+    res.status(200).json({ feedback: data });
+  }
+}
