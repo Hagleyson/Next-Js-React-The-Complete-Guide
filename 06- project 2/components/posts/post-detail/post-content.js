047599ba5dce538ed1e64114c20c0414e04155c2
hagleyson <hagleyson@gmail.com>
2022-03-17 16:18:10 -0300

    fix erros markdown

--- 06- project 2/components/posts/post-detail/post-content.js
+++ 06- project 2/components/posts/post-detail/post-content.js
@@ -1,22 +1,15 @@
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
+// import ReactMarkdown from "react-markdown";
+import ReactMarkdown from "react-markdown/react-markdown.min";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs",
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  excerpt: "NextJS is a the React framework for production",
-  date: "2022-03-17",
-  content: "# This is a first post",
-};
-export default function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+export default function PostContent(props) {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {/* <ReactMarkdown> */}
-      {DUMMY_POST.content}
-      {/* </ReactMarkdown> */}
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- 06- project 2/pages/posts/[slug].js
+++ 06- project 2/pages/posts/[slug].js
@@ -1,33 +1,31 @@
 import PostContent from "../../components/posts/post-detail/post-content";
-// import { getPostData, getPostsFiles } from "../../lib/posts.util";
+import { getPostData, getPostsFiles } from "../../lib/posts.util";
 
 export default function PostsDeatailPage(props) {
-  // console.log(props);
-  // return <h1>hagleyson</h1>;
-  return <PostContent />;
+  return <PostContent post={props.post} />;
 }
 
-// export function getStaticProps(context) {
-//   const { params } = context;
-//   const { slug } = params;
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
 
-//   const postData = getPostData(slug);
+  const postData = getPostData(slug);
 
-//   return {
-//     props: {
-//       post: postData,
-//     },
-//     revalidate: 600,
-//   };
-// }
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
 
-// export function getStaticPaths() {
-//   const postFilenames = getPostsFiles();
+export function getStaticPaths() {
+  const postFilenames = getPostsFiles();
 
-//   const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
-//   console.log(slugs);
-//   return {
-//     paths: slugs.map((slug) => ({ params: { slug: slug } })),
-//     fallback: false,
-//   };
-// }
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
+  console.log(slugs);
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
+}
