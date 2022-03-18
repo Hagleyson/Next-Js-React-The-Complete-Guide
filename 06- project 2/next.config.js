796c26155e136dc59e62100f8fc611499c305027
hagleyson <hagleyson@gmail.com>
2022-03-18 10:58:49 -0300

    preparing for deployment

---
+++ 06- project 2/next.config.js
@@ -0,0 +1,21 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "hagleyson",
+        mongodb_password: "password",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "my-site-dev",
+      },
+    };
+  }
+  return {
+    env: {
+      mongodb_username: "hagleyson",
+      mongodb_password: "password",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "my-site",
+    },
+  };
+};
--- 06- project 2/pages/api/contact.js
+++ 06- project 2/pages/api/contact.js
@@ -23,8 +23,9 @@ async function handler(req, res) {
     };
 
     let client;
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.ntrwp.mongodb.net/%${process.env.mongodb_database}?retryWrites=true&w=majority`;
     try {
-      client = await MongoClient.connect("mongourl");
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: "Could not connect to database." });
